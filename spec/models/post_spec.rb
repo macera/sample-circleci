1de4926081e7f65c17b84d8b72765ca967208b9a
Sera <sera@netlab.jp>
2017-05-27 15:59:09 +0900

    add success test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Post, type: :model do
 
 
   it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_length_of(:title).is_at_most(200) }
+  it { is_expected.to validate_length_of(:title).is_at_most(255) }
   it { is_expected.to validate_presence_of(:content) }
 
 end
